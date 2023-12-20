c2ec2f358b14982f37e95744ce866dde66c95e41
SurapureddySusmitha21 <surapureddysusmitha@gmail..com>
2023-12-20 15:10:28 +0000

    first commit

--- src/components/CoinToss/index.js
+++ src/components/CoinToss/index.js
@@ -4,51 +4,44 @@ import './index.css'
 
 const headImage = 'https://assets.ccbp.in/frontend/react-js/heads-img.png'
 const tailImage = 'https://assets.ccbp.in/frontend/react-js/tails-img.png'
+
 class CoinToss extends Component {
-  state = {headUrl: headImage, headCount: 0, tailCount: 0}
+  state = {heds: 0, tails: 0, image: headImage}
 
-  getClickTossCoin = () => {
-    const {headCount, tailCount} = this.state
-    const tossResult = Math.floor(Math.random() * 2)
-    let imgUrl = ''
-    let laterHeadCount = headCount
-    let laterFailsCount = tailCount
+  clickTheTosscoin = () => {
+    const randomCoin = Math.floor(Math.random() * 2)
 
-    if (tossResult === 0) {
-      imgUrl = headImage
-      laterHeadCount += 1
+    if (randomCoin === 0) {
+      this.setState({image: headImage})
+      this.setState(prevState => ({heds: prevState.heds + 1}))
     } else {
-      imgUrl = tailImage
-      laterFailsCount += 1
+      this.setState({image: tailImage})
+      this.setState(prevState => ({tails: prevState.tails + 1}))
     }
-    this.setState({
-      headUrl: imgUrl,
-      headCount: laterHeadCount,
-      tailCount: laterFailsCount,
-    })
   }
 
   render() {
-    const {headUrl, headCount, tailCount} = this.state
-    const total = headCount + tailCount
+    const {heds, tails, image} = this.state
+    const total = tails + heds
+
     return (
       <>
         <div className="bg-container">
           <div className="coin-container">
             <h1 className="heading">Coin Toss Game</h1>
             <p className="paragraph">Heads (or) Tails</p>
-            <img src={headUrl} className="URL" alt="toss result" />
+            <img src={image} alt="toss result" className="image-url" />
             <button
-              type="button"
               className="button"
-              onClick={this.getClickTossCoin}
+              type="button"
+              onClick={this.clickTheTosscoin}
             >
               Toss Coin
             </button>
             <div className="count-container">
-              <p className="count-paragraph">{`Total: ${total}`}</p>
-              <p className="count-paragraph">{`Heads: ${headCount}`}</p>
-              <p className="count-paragraph">{`Tails: ${tailCount}`}</p>
+              <p className="count-paragraph">Total: {total}</p>
+              <p className="count-paragraph">Heads: {heds}</p>
+              <p className="count-paragraph">Tails: {tails}</p>
             </div>
           </div>
         </div>
@@ -56,4 +49,5 @@ class CoinToss extends Component {
     )
   }
 }
+
 export default CoinToss
